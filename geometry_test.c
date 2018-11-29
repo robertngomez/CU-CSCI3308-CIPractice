96d1e2d288c7361b948f32be885248be9759f64d
Robert Gomez <rgomez@cu-engr2-1-13-10.201.49.210.int.colorado.edu>
2018-11-29 13:12:48 -0700

    Why cant i spell

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,28 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_coord_2d_area_triangle)
+{
+    coord_2d_t a,b,c;
+    a.x = 0;
+    a.y = 0;
+    b.x = 0;
+    b.y = 0;
+    c.x = 0;
+    c.y = 0;
+   ck_assert(coord_2d_area_triangle(a,b,c) == 0);
+
+   coord_2d_t a,b,c;
+    a.x = 0;
+    a.y = 5;
+    b.x = 5;
+    b.y = 0;
+    c.x = 0;
+    c.y = 0;
+   ck_assert(coord_2d_area_triangle(a,b,c) == 2.5);
+
+}
+END_TEST
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +184,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area");
+    tcase_add_test(tc_2d_area, test_coord_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
